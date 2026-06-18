adb7be512a84c5c7220296baed51bb3f39bf567a
CodeWizard0909 <shivangchaudhary789@gmail.com>
2026-06-18 16:11:15 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Uncomment the code below to pass the first stage
         System.out.print("$ ");
-
-        // Wait for user input
-        Scanner scanner = new Scanner(System.in);
-        scanner.nextLine();
     }
 }
